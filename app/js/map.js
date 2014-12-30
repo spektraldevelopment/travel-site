11b5ba5011748c81787ad9b3a448729a24125e52
SpektralDevelopment <spektraldevelopment@gmail.com>
2014-12-29 19:48:56 -0500

    Added basic info window.

--- app/js/map.js
+++ app/js/map.js
@@ -95,6 +95,28 @@
             });
         }
 
+        var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">' + obj.title + '</h1>'+
+            '<div id="bodyContent">'+
+            '<p>' + obj.description + '</p>'+
+            '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+            'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+            '(last visited June 22, 2009).</p>'+
+            '</div>'+
+            '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+//            map.setZoom(10);
+//            map.setCenter(marker.getPosition());
+            infowindow.open(map,marker);
+        });
+
         marker.setMap(map);
 
         if (obj.mostRecent === true) {
